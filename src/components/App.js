fbb9267b4cd33aa2e6d0b0ffa1371d45b93ad3a0
Laura Carbajales <laura.carbajales@gmail.com>
2022-01-26 16:00:09 +0100

    Add alphabetical order (Section 9)

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,17 @@ const App = () => {
 
   useEffect(() => {
     getApiData(filterHouse).then((charactersData) => {
-      setcharacters(charactersData);
+      setcharacters(
+        charactersData.sort(function (a, b) {
+          if (a.name > b.name) {
+            return 1;
+          }
+          if (a.name < b.name) {
+            return -1;
+          }
+          return 0;
+        })
+      );
     });
   }, [filterHouse]);
 
