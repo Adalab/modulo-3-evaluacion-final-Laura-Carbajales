571189927d1b244cd0ac49cd60cc2983b782e17a
Laura Carbajales <laura.carbajales@gmail.com>
2022-01-27 11:35:13 +0100

    Update Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,18 @@ import '../styles/components/Footer.scss';
 
 const Footer = (props) => {
   return (
-    <footer class='footer'>
-      <small class='footer__copy'>&copy; 2022 Laura Carbajales</small>
-      <section class='footer__rrss'>
-        <h3 class='footer__rrss--title'>See more about me on:</h3>
-        <a href='https://github.com/Laura-Carbajales' class='footer__rrss--link'>
-          <i class='icon fab fa-github-square'></i>
+    <footer className='footer'>
+      <small className='footer__copy'>&copy; 2022 Laura Carbajales</small>
+      <section className='footer__rrss'>
+        <h3 className='footer__rrss--title'>See more about me on:</h3>
+        <a href='https://github.com/Laura-Carbajales' className='footer__rrss--link'>
+          <i className='icon fab fa-github-square'></i>
         </a>
-        <a href='https://www.linkedin.com/in/lauracarbajales/' class='footer__rrss--link'>
-          <i class='icon fab fa-linkedin'></i>
+        <a href='https://www.linkedin.com/in/lauracarbajales/' className='footer__rrss--link'>
+          <i className='icon fab fa-linkedin'></i>
         </a>
-        <a href='https://twitter.com/Lau_1505' class='footer__rrss--link'>
-          <i class='icon fab fa-twitter-square'></i>
+        <a href='https://twitter.com/Lau_1505' className='footer__rrss--link'>
+          <i className='icon fab fa-twitter-square'></i>
         </a>
       </section>
     </footer>
